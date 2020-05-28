e582bb2c8fdafe6f267ecd6341971f157039e2bb
Rahulatheist <65806141+Rahulatheist@users.noreply.github.com>
2020-05-28 14:47:37 +0530

    Test case for spl video
    Test case for Learning To Ski video

--- OrderProcessingApplication/OderProcessingUnitTest/OderProcessingUnitTest.cs
+++ OrderProcessingApplication/OderProcessingUnitTest/OderProcessingUnitTest.cs
@@ -15,5 +15,15 @@ namespace OderProcessingUnitTest
             Assert.AreEqual(1, result.Operations.Count);
 
         }
+
+        [TestMethod]
+        public void ShouldReturnVideoLearningToSkiSlipOnly()
+        {
+            var result = OrderProcessor.ConvertInputToType(new string[] { "video", "Learning To Ski" });
+            Assert.AreEqual("Learning To Ski", result.ItemName);
+            Assert.AreEqual("Generated a packing slip.", result.Operations[0]);
+            Assert.AreEqual("'First Aid' video added to the packing slip", result.Operations[1]);
+            Assert.AreEqual(2, result.Operations.Count);
+        }
     }
 }
